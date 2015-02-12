4b574ae48d9132a084aed26eb6a44e31ccee2ade
Michael E Barreiro <Mikeb_barreiro@yahoo.com>
2015-02-12 16:59:59 -0600

    contact page

--- client/index.js
+++ client/index.js
@@ -1,17 +1,15 @@
 (function(){
   'use strict';
 
+// Here are the angular routes
+
   angular.module('mean-template', ['ngRoute', 'LocalForageModule'])
   .config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider){
     $routeProvider
     .when('/', {templateUrl:'/views/home/home.html', controller:'HomeCtrl'})
     .when('/contact', {templateUrl:'/views/contact/contact.html', controller:'ContactCtrl'})
-    // .when('/register', {templateUrl:'/views/register/register.html', controller:'RegisterCtrl'})
-    // .when('/login',    {templateUrl:'/views/login/login.html',       controller:'LoginCtrl'})
-    // .when('/logout',   {templateUrl:'/views/logout/logout.html',     controller:'LogoutCtrl'})
+// If user gets 400 or the other routes dont run the user is redirected to the home page here
     .otherwise({redirectTo:'/'});
 
-    // $httpProvider.interceptors.push('HttpInterceptor');
-    // $localForageProvider.config({name:'mean-template', storeName:'cache', version:1.0});
   }]);
 })();
--- client/views/contact/contact.js
+++ client/views/contact/contact.js
@@ -3,7 +3,13 @@
 
   angular.module('mean-template')
   .controller('ContactCtrl', ['$scope', function($scope){
+    // I create a scope called hideContent and make it true
+    $scope.hideContact = true;
 
+// this creates a scope function that makes hideContact from true to false.
+    $scope.toggleContact = function(){
+      $scope.hideContact = !!!$scope.hideContact;
+    };
 
   }]);
 })();
