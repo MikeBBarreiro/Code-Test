63f19511188a86012be828da432b2d83489dd6b1
Michael E Barreiro <Mikeb_barreiro@yahoo.com>
2015-02-12 04:08:49 -0600

    carousel

---
+++ public/index.js
@@ -0,0 +1,16 @@
+(function(){
+  'use strict';
+
+  angular.module('mean-template', ['ngRoute', 'LocalForageModule'])
+  .config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider){
+    $routeProvider
+    .when('/', {templateUrl:'/views/home/home.html', controller:'HomeCtrl'})
+    // .when('/register', {templateUrl:'/views/register/register.html', controller:'RegisterCtrl'})
+    // .when('/login',    {templateUrl:'/views/login/login.html',       controller:'LoginCtrl'})
+    // .when('/logout',   {templateUrl:'/views/logout/logout.html',     controller:'LogoutCtrl'})
+    .otherwise({redirectTo:'/'});
+
+    // $httpProvider.interceptors.push('HttpInterceptor');
+    // $localForageProvider.config({name:'mean-template', storeName:'cache', version:1.0});
+  }]);
+})();
